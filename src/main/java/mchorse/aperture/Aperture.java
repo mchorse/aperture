5dd54e1506f66770efdb6d78ea594387e519ff19
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-07-24 08:11:01 +0100

    Bump version

--- src/main/java/mchorse/aperture/Aperture.java
+++ src/main/java/mchorse/aperture/Aperture.java
@@ -23,7 +23,7 @@ public class Aperture
     /* Mod info */
     public static final String MODID = "aperture";
     public static final String MODNAME = "Aperture";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     public static final String GUI_FACTORY = "mchorse.aperture.config.gui.GuiFactory";
 
     /* Proxies */
