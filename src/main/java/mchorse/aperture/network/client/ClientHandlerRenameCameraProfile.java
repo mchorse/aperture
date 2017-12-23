21f75fd3ebd49ae39c2d75e12690ed84ecede1f1
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-12-23 08:41:49 +0000

    SideOnly fix for server

--- src/main/java/mchorse/aperture/network/client/ClientHandlerRenameCameraProfile.java
+++ src/main/java/mchorse/aperture/network/client/ClientHandlerRenameCameraProfile.java
@@ -4,10 +4,13 @@ import mchorse.aperture.ClientProxy;
 import mchorse.aperture.camera.destination.ServerDestination;
 import mchorse.aperture.network.common.PacketRenameCameraProfile;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ClientHandlerRenameCameraProfile extends ClientMessageHandler<PacketRenameCameraProfile>
 {
     @Override
+    @SideOnly(Side.CLIENT)
     public void run(EntityPlayerSP player, PacketRenameCameraProfile message)
     {
         ClientProxy.cameraEditor.profiles.rename(new ServerDestination(message.from), message.to);
