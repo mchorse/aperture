533fd59e7b351b292431a93f61f9d54657355d4b
McHorse <mchorselessrider@gmail.com>
2019-02-05 10:49:19 +0000

    1.11.2 oof

--- src/main/java/mchorse/aperture/client/gui/GuiCameraEditor.java
+++ src/main/java/mchorse/aperture/client/gui/GuiCameraEditor.java
@@ -833,7 +833,7 @@ public class GuiCameraEditor extends GuiBase implements IScrubListener
 
         if (this.lastGameMode != GameType.NOT_SET)
         {
-            this.mc.thePlayer.sendChatMessage("/gamemode " + this.lastGameMode.getID());
+            this.mc.player.sendChatMessage("/gamemode " + this.lastGameMode.getID());
         }
 
         super.closeScreen();
