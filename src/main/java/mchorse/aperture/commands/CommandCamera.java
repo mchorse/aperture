e8207cdcc632a95acca64755edab59f2fe0e6325
McHorse <mchorselessrider@gmail.com>
2019-02-16 08:32:40 +0000

    Remove old /camera sub-commands related to editing the camera profile

--- src/main/java/mchorse/aperture/commands/CommandCamera.java
+++ src/main/java/mchorse/aperture/commands/CommandCamera.java
@@ -1,12 +1,6 @@
 package mchorse.aperture.commands;
 
 import mchorse.aperture.Aperture;
-import mchorse.aperture.commands.camera.SubCommandCameraClear;
-import mchorse.aperture.commands.camera.SubCommandCameraClose;
-import mchorse.aperture.commands.camera.SubCommandCameraList;
-import mchorse.aperture.commands.camera.SubCommandCameraLoad;
-import mchorse.aperture.commands.camera.SubCommandCameraNew;
-import mchorse.aperture.commands.camera.SubCommandCameraSave;
 import mchorse.aperture.commands.camera.SubCommandCameraStart;
 import mchorse.aperture.commands.camera.SubCommandCameraStop;
 import mchorse.aperture.commands.camera.control.SubCommandCameraDefault;
@@ -14,13 +8,6 @@ import mchorse.aperture.commands.camera.control.SubCommandCameraFOV;
 import mchorse.aperture.commands.camera.control.SubCommandCameraRoll;
 import mchorse.aperture.commands.camera.control.SubCommandCameraRotate;
 import mchorse.aperture.commands.camera.control.SubCommandCameraStep;
-import mchorse.aperture.commands.fixture.SubCommandFixtureAdd;
-import mchorse.aperture.commands.fixture.SubCommandFixtureDuration;
-import mchorse.aperture.commands.fixture.SubCommandFixtureEdit;
-import mchorse.aperture.commands.fixture.SubCommandFixtureGoto;
-import mchorse.aperture.commands.fixture.SubCommandFixtureMove;
-import mchorse.aperture.commands.fixture.SubCommandFixturePath;
-import mchorse.aperture.commands.fixture.SubCommandFixtureRemove;
 
 /**
  * Command /camera
@@ -48,31 +35,12 @@ public class CommandCamera extends SubCommandBase
         this.add(new SubCommandCameraStart());
         this.add(new SubCommandCameraStop());
 
-        /* Load/save */
-        this.add(new SubCommandCameraNew());
-        this.add(new SubCommandCameraLoad());
-        this.add(new SubCommandCameraSave());
-
-        /* Profile */
-        this.add(new SubCommandCameraClear());
-        this.add(new SubCommandCameraList());
-        this.add(new SubCommandCameraClose());
-
         /* Camera control */
         this.add(new SubCommandCameraStep());
         this.add(new SubCommandCameraRotate());
         this.add(new SubCommandCameraRoll());
         this.add(new SubCommandCameraFOV());
         this.add(new SubCommandCameraDefault());
-
-        /* Fixture editing */
-        this.add(new SubCommandFixtureAdd());
-        this.add(new SubCommandFixtureEdit());
-        this.add(new SubCommandFixtureRemove());
-        this.add(new SubCommandFixtureMove());
-        this.add(new SubCommandFixtureGoto());
-        this.add(new SubCommandFixtureDuration());
-        this.add(new SubCommandFixturePath());
     }
 
     @Override
