1e63a78a751e21b6190664b9f0ba0ad22fa14d96
McHorse <mchorselessrider@gmail.com>
2019-10-03 19:32:30 +0100

    Improved flight mode speed to go lower than 0.1 and inverted factor (suggested by Kanguste)

--- src/main/java/mchorse/aperture/client/gui/GuiCameraEditor.java
+++ src/main/java/mchorse/aperture/client/gui/GuiCameraEditor.java
@@ -979,8 +979,24 @@ public class GuiCameraEditor extends GuiBase implements IScrubListener
 
         if (this.flight.enabled)
         {
-            this.flight.speed += Math.copySign(0.1F, scroll);
-            this.flight.speed = MathHelper.clamp_float(this.flight.speed, 0.1F, 50F);
+            float factor = 1000;
+            boolean zoomIn = scroll > 0;
+
+            if ((zoomIn && this.flight.speed <= 10) || (!zoomIn && this.flight.speed < 10))
+            {
+                factor = 1;
+            }
+            else if ((zoomIn && this.flight.speed <= 100) || (!zoomIn && this.flight.speed < 100))
+            {
+                factor = 10;
+            }
+            else if ((zoomIn && this.flight.speed <= 1000) || (!zoomIn && this.flight.speed < 1000))
+            {
+                factor = 100;
+            }
+
+            this.flight.speed -= Math.copySign(factor, scroll);
+            this.flight.speed = MathHelper.clamp_int(this.flight.speed, 1, 50000);
         }
     }
 
@@ -1152,7 +1168,14 @@ public class GuiCameraEditor extends GuiBase implements IScrubListener
         /* Display flight speed */
         if (this.flight.enabled)
         {
-            String speed = String.format(this.stringSpeed + ": %.1f", this.flight.speed);
+            float flightSpeed = this.flight.speed / 1000F;
+            String speedFormat = "%.0f";
+
+            if (flightSpeed < 0.01F) speedFormat = "%.3f";
+            else if (flightSpeed < 0.1F) speedFormat = "%.2f";
+            else if (flightSpeed < 1F) speedFormat = "%.1f";
+
+            String speed = String.format(this.stringSpeed + ": " + speedFormat, flightSpeed);
             int width = this.fontRendererObj.getStringWidth(speed);
             int x = panel.getX() + panel.getW() - 10 - width;
             int y = panel.getY() + panel.getH() - 5;
