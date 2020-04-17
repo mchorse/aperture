e7ffe4668e9a21c882ee2b18c76be3ed5f61dfed
McHorse <mchorselessrider@gmail.com>
2020-04-17 08:25:03 +0100

    Remove stop camera keybind and make playback keybind to toggle playback

--- src/main/java/mchorse/aperture/client/KeyboardHandler.java
+++ src/main/java/mchorse/aperture/client/KeyboardHandler.java
@@ -45,8 +45,7 @@ public class KeyboardHandler
 
     /* Camera profile keys */
     private KeyBinding toggleRender;
-    private KeyBinding startRunning;
-    private KeyBinding stopRunning;
+    private KeyBinding toggleRunning;
     private KeyBinding addPoint;
 
     /* Roll and FOV */
@@ -111,13 +110,11 @@ public class KeyboardHandler
         String misc = "key.aperture.misc";
 
         this.toggleRender = new KeyBinding("key.aperture.profile.toggle", Keyboard.KEY_P, profile);
-        this.startRunning = new KeyBinding("key.aperture.profile.start", Keyboard.KEY_Z, profile);
-        this.stopRunning = new KeyBinding("key.aperture.profile.stop", Keyboard.KEY_X, profile);
+        this.toggleRunning = new KeyBinding("key.aperture.profile.playback", Keyboard.KEY_Z, profile);
         this.addPoint = new KeyBinding("key.aperture.profile.point", Keyboard.KEY_NONE, profile);
 
         ClientRegistry.registerKeyBinding(this.toggleRender);
-        ClientRegistry.registerKeyBinding(this.startRunning);
-        ClientRegistry.registerKeyBinding(this.stopRunning);
+        ClientRegistry.registerKeyBinding(this.toggleRunning);
         ClientRegistry.registerKeyBinding(this.addPoint);
 
         /* Roll and FOV */
@@ -256,14 +253,10 @@ public class KeyboardHandler
             ClientProxy.renderer.toggleRender();
         }
 
-        /* Starting stopping */
-        if (this.startRunning.isPressed())
+        /* Starting/stopping */
+        if (this.toggleRunning.isPressed())
         {
-            ClientProxy.runner.start(ClientProxy.control.currentProfile);
-        }
-        else if (this.stopRunning.isPressed())
-        {
-            ClientProxy.runner.stop();
+            ClientProxy.runner.toggle(ClientProxy.control.currentProfile, 0);
         }
 
         if (this.resetRoll.isPressed())
